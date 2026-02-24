1b3b0c15c0d56cac0b356b9f4aa66e05b9c0a1d8
stuff7563 <felix2906@gmx.de>
2026-02-24 22:05:49 +0100

    Center logo inside circle and adjust size

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ function stripLocale(pathname: string) {
  * z.B. LOGO_NUDGE_X = "translate-x-[5px]" oder "-translate-x-[1px]"
  *      LOGO_NUDGE_Y = "translate-y-[0px]" oder "-translate-y-[1px]"
  */
-const LOGO_NUDGE_X = "translate-x-[1px]";
+const LOGO_NUDGE_X = "translate-x-[3px]";
 const LOGO_NUDGE_Y = "translate-y-[1px]";
 function LogoCircle({ size }: { size: "mobile" | "desktop" }) {
   const outer = size === "mobile" ? "h-14 w-14" : "h-14 w-14"; // Desktop auch 48px, kannst du auf h-14 w-14 erhöhen
